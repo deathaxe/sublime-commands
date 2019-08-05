8baff65de7368641b3604611fadb131d2818c733
Robert Laverty <roblav96@users.noreply.github.com>
2019-08-04 22:19:42 -0400

    Update toggle.py

--- toggle.py
+++ toggle.py
@@ -10,6 +10,7 @@ class ToggleLogBuiltSystemsCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         self.flag = not self.flag
         sublime.log_build_systems(self.flag)
+        sublime.status_message('log_build_systems -> %s' % str(self.flag))
 
 
 class ToggleLogCommandsCommand(sublime_plugin.ApplicationCommand):
@@ -20,6 +21,7 @@ class ToggleLogCommandsCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         self.flag = not self.flag
         sublime.log_commands(self.flag)
+        sublime.status_message('log_commands -> %s' % str(self.flag))
 
 
 class ToggleLogInputCommand(sublime_plugin.ApplicationCommand):
@@ -30,6 +32,7 @@ class ToggleLogInputCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         self.flag = not self.flag
         sublime.log_input(self.flag)
+        sublime.status_message('log_input -> %s' % str(self.flag))
 
 
 class ToggleLogIndexingCommand(sublime_plugin.ApplicationCommand):
@@ -40,6 +43,7 @@ class ToggleLogIndexingCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         self.flag = not self.flag
         sublime.log_indexing(self.flag)
+        sublime.status_message('log_indexing -> %s' % str(self.flag))
 
 
 class ToggleLogResultRegexCommand(sublime_plugin.ApplicationCommand):
@@ -50,6 +54,7 @@ class ToggleLogResultRegexCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         self.flag = not self.flag
         sublime.log_result_regex(self.flag)
+        sublime.status_message('log_result_regex -> %s' % str(self.flag))
 
 
 class TogglePreferenceCommand(sublime_plugin.ApplicationCommand):
