957ad280d00a97b492f028e1506e85b939a72491
DeathAxe <deathaxe82@googlemail.com>
2022-05-07 17:16:38 +0200

    Dynamically load list of supported encodings

--- encoding.py
+++ encoding.py
@@ -1,3 +1,4 @@
+import sublime
 import sublime_plugin
 
 
@@ -11,41 +12,10 @@ class EncodingInputHandler(sublime_plugin.ListInputHandler):
 
     def list_items(self):
         return [
-            "UTF-8",
-            "UTF-8 with BOM",
-            "UTF-16 LE",
-            "UTF-16 LE with BOM",
-            "UTF-16 BE",
-            "UTF-16 BE with BOM",
-            "Western (Windows 1252)",
-            "Western (ISO 8859-1)",
-            "Western (ISO 8859-3)",
-            "Western (ISO 8859-15)",
-            "Western (Mac Roman)",
-            "DOS (CP 437)",
-            "Arabic (Windows 1256)",
-            "Arabic (ISO 8859-6)",
-            "Baltic (Windows 1257)",
-            "Baltic (ISO 8859-4)",
-            "Celtic (ISO 8859-14)",
-            "Central European (Windows 1250)",
-            "Central European (ISO 8859-2)",
-            "Cyrillic (Windows 1251)",
-            "Cyrillic (Windows 866)",
-            "Cyrillic (ISO 8859-5)",
-            "Cyrillic (KOI8-R)",
-            "Cyrillic (KOI8-U)",
-            "Estonian (ISO 8859-13)",
-            "Greek (Windows 1253)",
-            "Greek (ISO 8859-7)",
-            "Hebrew (Windows 1255)",
-            "Hebrew (ISO 8859-8)",
-            "Nordic (ISO 8859-10)",
-            "Romanian (ISO 8859-16)",
-            "Turkish (Windows 1254)",
-            "Turkish (ISO 8859-9)",
-            "Vietnamese (Windows 1258)",
-            "Hexadecimal"
+            item["args"]["encoding"]
+            for item in sublime.decode_value(sublime.load_resource(
+                "Packages/Default/Encoding.sublime-menu"))[0]["children"]
+            if "args" in item
         ]
 
 
