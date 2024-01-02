0357ece322a195a94845cf7a4fa127bea8a2dc02
deathaxe <deathaxe82@googlemail.com>
2024-01-02 19:14:44 +0100

    Remove extra commands as they will go into a separate package

--- clear_undo_stack.py
+++
@@ -1,8 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class ClearUndoStackCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.active_view().clear_undo_stack()
-        sublime.status_message("Undo Stack of the current file has been cleared")
