36c9db6d173161f3d3a2f4a4d9c961c8112d3189
deathaxe <deathaxe82@gmail.com>
2026-06-25 19:13:12 +0200

    Exec: Protect against theoretically possible None access exception

--- exec.py
+++ exec.py
@@ -55,14 +55,14 @@ class AsyncProcess:
 
         try:
             # Set temporary PATH to locate executable in cmd
-            if "PATH" in env:
-                old_path = os.environ["PATH"]
-                # The user decides in the build system whether he wants to append
-                # $PATH or tuck it at the front: "$PATH;C:\\new\\path",
-                # "C:\\new\\path;$PATH"
-                os.environ["PATH"] = os.path.expandvars(env["PATH"])
-
             if env:
+                if "PATH" in env:
+                    old_path = os.environ["PATH"]
+                    # The user decides in the build system whether he wants to append
+                    # $PATH or tuck it at the front: "$PATH;C:\\new\\path",
+                    # "C:\\new\\path;$PATH"
+                    os.environ["PATH"] = os.path.expandvars(env["PATH"])
+
                 proc_env = os.environ.copy()
                 for k, v in env.items():
                     proc_env[k] = os.path.expandvars(v)
